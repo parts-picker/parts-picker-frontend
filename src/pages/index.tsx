f451364ab2c914bed8332c97ba9a5dc3d27801c7
RobinGuth <robin.guth@gmx.de>
2022-08-02 22:59:47 +0200

    PICK-UI-53: Add sass as dev dependency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
-import styles from "../../styles/Home.module.css";
 import { H1 } from "@blueprintjs/core";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>parts picker</title>
         <meta name="description" content="Early development version." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <H1>parts picker</H1>
       </main>
     </div>
